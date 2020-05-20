96da763eccb40d38b7e02fe21cdb8586e78bce33
nwgat <github@nwgat.net>
2020-05-20 11:14:40 +0200

    Update ics-setup.bat

--- ics-setup.bat
+++ ics-setup.bat
@@ -1,3 +1,4 @@
 mkdir C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ics\
 copy ics.psm1 C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ics\
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Import-Module ics
 regedit.exe /S ics_persist.reg
