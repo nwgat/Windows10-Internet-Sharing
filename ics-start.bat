ef241801c82d0d23506dc17acb3b85245f72720d
nwgat <github@nwgat.net>
2020-05-20 11:00:37 +0200

    Update ics-start.bat

--- ics-start.bat
+++ ics-start.bat
@@ -1,1 +1,3 @@
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-Service SharedAccess -StartupType Automatic
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Start-Service SharedAccess
 "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-NetConnectionSharing "nameofnetworktoshareto" $true
