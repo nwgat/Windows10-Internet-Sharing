ca53f68578099c72007bd0f57ec083545e7a3b9b
nwgat <github@nwgat.net>
2020-05-20 11:01:14 +0200

    Update ics-stop.bat

--- ics-stop.bat
+++ ics-stop.bat
@@ -1,1 +1,3 @@
 "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-NetConnectionSharing "nameofnetworktoshareto" $false
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-Service SharedAccess -StartupType Disabled
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Stop-Service SharedAccess
