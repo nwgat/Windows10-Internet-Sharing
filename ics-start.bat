30e312200869d1377bdb0e40a81078a8378f4e48
nwgat <github@nwgat.net>
2020-05-26 19:36:18 +0200

    Update ics-start.bat

--- ics-start.bat
+++ ics-start.bat
@@ -1,3 +1,7 @@
+# nwgat.ninja
+# https://nwgat.ninja/win10-ics
+
+set /p network=Network to share internet connection to?:
 "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-Service SharedAccess -StartupType Automatic
 "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Start-Service SharedAccess
-"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-NetConnectionSharing "nameofnetworktoshareto" $true
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-NetConnectionSharing "%network%" $true
