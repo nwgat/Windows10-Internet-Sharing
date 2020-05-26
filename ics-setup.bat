d0bdac34ad0299d86fbf73b01898ea853ef631e0
nwgat <github@nwgat.net>
2020-05-26 19:37:43 +0200

    Update ics-setup.bat

--- ics-setup.bat
+++ ics-setup.bat
@@ -1,3 +1,6 @@
+# nwgat.ninja
+# https://nwgat.ninja/win10-ics
+
 mkdir "%SystemRoot%\System32\WindowsPowerShell\v1.0\Modules\ics\
 copy ics.psm1 C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ics\
 "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Import-Module ics
