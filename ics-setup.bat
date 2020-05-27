0f1dac86280550fa7695867b9a8a9222d21be4d1
nwgat <github@nwgat.net>
2020-05-28 01:23:25 +0200

    Delete ics-setup.bat

--- ics-setup.bat
+++
@@ -1,7 +0,0 @@
-# nwgat.ninja
-# https://nwgat.ninja/win10-ics
-
-mkdir "%SystemRoot%\System32\WindowsPowerShell\v1.0\Modules\ics\
-copy ics.psm1 C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ics\
-"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Import-Module ics
-regedit.exe /S ics_persist.reg
