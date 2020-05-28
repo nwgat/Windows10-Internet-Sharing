dd75e052f743a731df047c10d991db21116fbb8e
nwgat <github@nwgat.net>
2020-05-28 08:58:09 +0200

    Add files via upload

--- ics-install.bat
+++ ics-install.bat
@@ -1,9 +1,9 @@
 @echo nwgat.ninja
 @echo ics-setup
 @echo https://nwgat.ninja/win10-ics
-
-mkdir "%SystemRoot%\System32\WindowsPowerShell\v1.0\Modules\ics\
-copy ics.psm1 C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ics\
-"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"  Set-ExecutionPolicy Unrestricted
-"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"  Import-Module ics
+mkdir "%SystemRoot%\System32\WindowsPowerShell\v1.0\Modules\ics\"
+copy "%~dp0\ics.psm1" "C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ics\"
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-ExecutionPolicy Unrestricted
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Import-Module ics
 regedit.exe /S ics_persist.reg
+pause
