bec848054d4615be1304b7823405aec01f99317d
nwgat <github@nwgat.net>
2020-06-02 22:54:51 +0200

    Update ics-stop.bat

--- ics-stop.bat
+++ ics-stop.bat
@@ -1,6 +1,7 @@
-# nwgat.ninja
-# https://nwgat.ninja/win10-ics
+@echo nwgat.ninja
+@echo https://nwgat.ninja/win10-ics
 
-"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-NetConnectionSharing "nameofnetworktoshareto" $false
+set /p network=What network to stop sharing internet connection to?:
+"%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-NetConnectionSharing "%network%" $false
 "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Set-Service SharedAccess -StartupType Disabled
 "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe" Stop-Service SharedAccess
